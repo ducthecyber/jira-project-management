05e8e480e8bcb114723a2d9c39aa9754fb92b2f8
Ty-Doan <tydoan.28052001@gmail.com>
2022-12-03 13:26:24 +0700

    push repair project management code + repair PhanChiaCongViec

--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -1,31 +1,44 @@
 import { USER_LOGIN_KEY } from "../../constants/common"
 import { GET_USER_LIST, SET_USER_INFO } from "../types/userType"
+import { EDIT_USER, GET_USER, GET_USER_BY_PROJECT } from "../types/userType";
 
 //localStorage
 let userInfo = localStorage.getItem(USER_LOGIN_KEY);
 
-if(userInfo){
-    userInfo= JSON.parse(userInfo)
+if (userInfo) {
+    userInfo = JSON.parse(userInfo)
 }
 
 const stateDefault = {
-    userInfo:userInfo,
-    arrUser:[],
+    userInfo: userInfo,
+    arrUser: [],
+    userByProject: [],
+    arrUser: [],
+    userEdit: {},
 }
 
-export const userReducer = (state= stateDefault,{payload,type})=>{
-    switch(type){
-        case SET_USER_INFO :{
-            let data = {...state.userInfo}
+export const userReducer = (state = stateDefault, { payload, type }) => {
+    switch (type) {
+        case SET_USER_INFO: {
+            let data = { ...state.userInfo }
             data = payload
-            localStorage.setItem(SET_USER_INFO,JSON.stringify(data))
-            return {...state,userInfo:data}
+            localStorage.setItem(SET_USER_INFO, JSON.stringify(data))
+            return { ...state, userInfo: data }
         }
-        case GET_USER_LIST:{
-            let data = {...state.arrUser}
+        case GET_USER_LIST: {
+            let data = { ...state.arrUser }
             data = payload
-            return{...state,arrUser:data}
+            return { ...state, arrUser: data }
         }
-        default:return state
+        case GET_USER: {
+            return { ...state, userSearch: payload }
+        }
+        case GET_USER_BY_PROJECT: {
+            return { ...state, userByProject: payload }
+        }
+        case EDIT_USER: {
+            return { ...state, userEdit: payload }
+        }
+        default: return state
     }
 }
